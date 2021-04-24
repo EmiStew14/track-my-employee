0a58dc2c6f8ce826045d06b9867154e5c71d9531
Emily Stewart <stewartemilym2014@gmail.com>
2021-04-23 21:25:52 -0700

    filed out sql files with appropriate tables, and created route files

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS ;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+);
